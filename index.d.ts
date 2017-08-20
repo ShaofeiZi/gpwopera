30bd0504457aea8468e528ca28c622101c564238
訾绍飞 <zishaofei221@gmail.com>
2017-08-20 13:14:21 +0800

    自用

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+interface Number {
+    accAdd(arg1: number, arg2: number): number;
+    accSub(arg1: number, arg2: number): number;
+    accMul(arg1: number, arg2: number): number;
+    accDiv(arg1: number, arg2: number): number;
+}
